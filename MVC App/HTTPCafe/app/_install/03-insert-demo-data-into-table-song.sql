842e97f4a0abf528ac6fef880de7603b28fb31e0
LaviniaDisca <48357956+LaviniaDisca@users.noreply.github.com>
2019-04-22 18:41:11 +0300

    Products inserted

--- MVC App/HTTPCafe/app/_install/03-insert-demo-data-into-table-song.sql
+++ MVC App/HTTPCafe/app/_install/03-insert-demo-data-into-table-song.sql
@@ -1,1 +1,55 @@
-INSERT INTO ...
+INSERT INTO `products` (`id`, `type`, `name`, `photo_path`, `price`, `stock`) VALUES ('1', 'coffee', 'Cappuccino', 'cappuccino.jpg', '5.99', '100');
+
+INSERT INTO `products` (`id`, `type`, `name`, `photo_path`, `price`, `stock`) VALUES ('2', 'coffee', 'Iced coffee', 'Iced-coffee.jpg', '7.99', '89');
+
+INSERT INTO `products` (`id`, `type`, `name`, `photo_path`, `price`, `stock`) VALUES ('3', 'coffee', 'Americano', 'americano.jpg', '4.99', '95');
+
+INSERT INTO `products` (`id`, `type`, `name`, `photo_path`, `price`, `stock`) VALUES ('4', 'coffee', 'Irish Coffee', 'irishCoffee.jpg', '8.50', '77'),
+('5', 'coffee', 'Vienna Coffee', 'vienna-coffee.jpg', '9.99', '64');
+
+INSERT INTO `products` (`id`, `type`, `name`, `photo_path`, `price`, `stock`) VALUES ('6', 'coffee', 'Caffé Latte', 'caffe-late.jpg', '4.85', '110'),
+ ('7', 'coffee', 'Espresso', 'espresso.jpg', '2.99', '150');
+
+INSERT INTO `products` (`id`, `type`, `name`, `photo_path`, `price`, `stock`) VALUES ('8', 'fresh', 'Hibiscus Strawberry Rhubarb Iced Tea', 'strawberry.png', '8.50', '50'),
+('9', 'fresh', 'Fruit Punch Lemonade', 'punch.jpg', '5.90', '70');
+
+INSERT INTO `products` (`id`, `type`, `name`, `photo_path`, `price`, `stock`) VALUES ('10', 'fresh', 'Pineapple Ginger Sparkler', 'pin.jpg', '5.00', '48'),
+ ('11', 'fresh', 'Paloma Fizz Mocktails', 'paloma.jpg', '12.99', '35'),
+ ('12', 'fresh', 'Sparkling Mint-Lime Iced Tea', 'ice.jpg', '6.79', '80'),
+ ('13', 'fresh', 'Basil Lemonade Punch', 'lemonade.jpg', '9.99', '79'),
+ ('14', 'fresh', 'Pomegranate mocktail', 'pome.jpg', '14.99', '55');
+
+INSERT INTO `products` (`id`, `type`, `name`, `photo_path`, `price`, `stock`) VALUES
+('15', 'mini-cake', 'Red Velvet', 'velvet.jpg', '8.99', '30'),
+('16', 'mini-cake', 'Butterbeer Cupcakes', 'butter.jpg', '10.99', '25'),
+('17', 'mini-cake', 'Chocolate Chip Cookie Dough Cheesecake', 'chip.jpg', '12.99', '33'),
+('18', 'mini-cake', 'Oreo Cheesecake', 'oreo.jpg', '11.98', '19'),
+('19', 'mini-cake', 'Chocolate-Peanut Butter Cheesecake', 'peanut.jpg', '15.99', '25'),
+('20', 'mini-cake', 'Brownies', 'brownie.jpg', '5.50', '50'),
+('21', 'mini-cake', 'Hot Fudge Cheesecake', 'fudge.jpg', '9.49', '30'),
+('22', 'mini-cake', 'Tiramisu Cheesecake', 'tiramisu.jpg', '11.99', '28');
+
+INSERT INTO `products` (`id`, `type`, `name`, `photo_path`, `price`, `stock`) VALUES
+('23', 'cookie', 'Chocolate Chip Cookies', 'chocochip.jpg', '0.99', '250'),
+('24', 'cookie', 'Healthy Five-Ingredient Granola Bar', 'granola.jpg', '1.50', '244'),
+('25', 'cookie', 'Peanut Butter Oat Snack Cookies', 'oats.jpg', '1.55', '160'),
+('26', 'cookie', 'Macaroons', 'Macaroons.jpg', '2.99', '190'),
+('27', 'cookie', 'Madeleines', 'madeleines.jpg', '0.79', '120'),
+('28', 'cookie', 'Frosted Cookies', 'frost.jpg', '1.99', '178');
+
+
+INSERT INTO `products` (`id`, `type`, `name`, `photo_path`, `price`, `stock`) VALUES
+('29', 'pretzel', 'Pretzels', 'pretz.jpg', '3.88', '110'),
+('30', 'pretzel', 'Croissants', 'croissant.jpg', '3.50', '70'),
+('31', 'pretzel', 'Donuts', 'donut.jpg', '5.99', '100'),
+('32', 'pretzel', 'Breadsticks', 'sticks.jpg', '0.59', '120'),
+('33', 'pretzel', 'Apple Strudel', 'strudel.jpg', '7.99', '55'),
+('34', 'pretzel', 'Pear Strudel', 'pear.jpg', '7.99', '40');
+
+INSERT INTO `products` (`id`, `type`, `name`, `photo_path`, `price`, `stock`) VALUES
+('35', 'merch', 'Coffee Mug', 'cofmug.jpg', '12.99', '26'),
+('36', 'merch', 'Programmer Mug', 'cofmug2.jpg', '15.99', '25'),
+('37', 'merch', 'Bug Mug', 'cofmug3.jpg', '18.99', '30'),
+('38', 'merch', 'The shirt of truth', 'falseShirt.jpg', '20.99', '30'),
+('39', 'merch', 'Code Mug', 'cofcode.jpg', '14.99', '22'),
+('40', 'merch', 'Code Shirt', 'cofcodeshirt.jpg', '20.99', '16');
