26747d2278fbdc1ff1501bd63604e2dd67b3ad5e
mariusBlaj47 <47899839+mariusBlaj47@users.noreply.github.com>
2019-04-27 20:40:15 +0300

    One style to rule them all

--- MVC App/HTTPCafe/app/views/cookies/index.php
+++ MVC App/HTTPCafe/app/views/cookies/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <title>Cookies</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?php echo URL?>public/css/Cookies-style.css">
+    <link rel="stylesheet" href="<?php echo URL?>public/css/Products-style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
           integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
--- MVC App/HTTPCafe/app/views/pretzels/index.php
+++ MVC App/HTTPCafe/app/views/pretzels/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <title>Pretzels</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?php echo URL?>public/css/Pretzels-style.css">
+    <link rel="stylesheet" href="<?php echo URL?>public/css/Products-style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
           integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
@@ -22,7 +22,7 @@
                     <span class="nav-icon"><i class="fas fa-home"></i></span>
                     <span class="nav-option">Home</span>
                 </a></li>
-            <li><a href="<?php echo URL?>Coffees">
+            <li><a>
                     <span class="nav-icon"><i class="fab fa-elementor"></i></span>
                     <span class="nav-option">Menu</span>
                 </a >
